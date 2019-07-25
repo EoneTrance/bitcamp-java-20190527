8bec1de62ce0b4757c08acc4e387d8dc6192d894
EoneTrance <karnenis@gmail.com>
2019-07-25 15:47:43 +0900

    bitcamp-java-application v29

---
+++ bitcamp-java-application/v29/src/main/java/practice/interfacePractice2/Iterable.java
@@ -0,0 +1,7 @@
+package practice.interfacePractice2;
+
+public interface Iterable<E> {
+  
+  Iterator<E> iterator ();
+
+}
---
+++ bitcamp-java-application/v29/src/main/java/practice/interfacePractice2/Queue.java
@@ -0,0 +1,46 @@
+package practice.interfacePractice2;
+
+public class Queue<E> extends LinkedList<E> implements Cloneable, Iterable<E>{
+  
+  @Override
+  public Queue<E> clone() throws CloneNotSupportedException {
+    
+    Queue<E> temp = new Queue<>();
+    
+    for (int i = 0; i < size(); i++) {
+      temp.offer(get(i));
+    }
+    
+    return temp;
+    
+  }
+  
+  public void offer(E value) {
+    add(value);
+  }
+  
+  public E poll() {
+    return remove(0);
+  }
+  
+  public boolean empty() {
+    return size() == 0;
+  }
+  
+  @Override
+  public Iterator<E> iterator() {
+    
+    return new Iterator<E>() {
+      @Override
+      public boolean hasNext() {
+        return size() > 0;
+      }
+      
+      @Override
+      public E next() {
+        return poll();
+      }
+    };
+    
+  }
+}
